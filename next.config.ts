4068d9d4b40133179ff18c9a2e156f825345cbc6
Sudil Lakindu Mallika Arachchi <masudillakindu@gmail.com>
2026-02-24 00:51:34 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-            },
-            {
-                protocol: "https",
-                hostname: "plus.unsplash.com",
-            },
-        ],
-    },
+  /* config options here */
 };
 
 export default nextConfig;
